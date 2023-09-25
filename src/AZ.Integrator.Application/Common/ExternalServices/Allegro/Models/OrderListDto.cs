33b52ca6d5ff4803facc0d60f7145583034b1d02
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-09-25 11:31:14 +0200

    Extend OrderListDto model

--- src/AZ.Integrator.Application/Common/ExternalServices/Allegro/Models/OrderListDto.cs
+++ src/AZ.Integrator.Application/Common/ExternalServices/Allegro/Models/OrderListDto.cs
@@ -5,6 +5,7 @@ public class OrderListDto
     public Guid OrderId { get; set; }
     public DateTime Date { get; set; }
     public BuyerDto Buyer { get; set; }
+    public List<LineItemDto> LineItems { get; set; }
 }
 
 public class BuyerDto
@@ -13,4 +14,21 @@ public class BuyerDto
     public string Email { get; set; }
     public string Login { get; set; }
     public bool Guest { get; set; }
+}
+
+public class LineItemDto
+{
+    public string Id { get; set; }
+    public string ItemId { get; set; }
+    public string ItemName { get; set; }
+    public int Quantity { get; set; }
+    public PriceDto OriginalPrice { get; set; }
+    public PriceDto Price { get; set; }
+    public DateTime BoughtAt { get; set; }
+}
+
+public class PriceDto
+{
+    public string Amount { get; set; }
+    public string Currency { get; set; }
 }
