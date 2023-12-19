a96eb0b2bff96e4073d8a85156f82e43ab4a28f5
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2023-12-19 12:28:10 +0100

    Save Parcel Template for Tag logic

--- src/TagParcelTemplates/AZ.Integrator.TagParcelTemplates.Application/UseCases/Commands/SaveParcelTemplate/SaveParcelTemplateCommandHandler.cs
+++ src/TagParcelTemplates/AZ.Integrator.TagParcelTemplates.Application/UseCases/Commands/SaveParcelTemplate/SaveParcelTemplateCommandHandler.cs
@@ -1,16 +1,53 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using AZ.Integrator.Domain.Abstractions;
+using AZ.Integrator.TagParcelTemplates.Domain.Aggregates.TagParcelTemplate;
+using AZ.Integrator.TagParcelTemplates.Domain.Aggregates.TagParcelTemplate.Specifications;
+using AZ.Integrator.TagParcelTemplates.Domain.Aggregates.TagParcelTemplate.ValueObjects;
+using MediatR;
 
 namespace AZ.Integrator.TagParcelTemplates.Application.UseCases.Commands.SaveParcelTemplate;
 
 public class SaveParcelTemplateCommandHandler : IRequestHandler<SaveParcelTemplateCommand>
 {
-    public SaveParcelTemplateCommandHandler()
+    private readonly IAggregateRepository<TagParcelTemplate> _repository;
+    private readonly IMapper _mapper;
+    private readonly ICurrentUser _currentUser;
+    private readonly ICurrentDateTime _currentDateTime;
+
+    public SaveParcelTemplateCommandHandler(
+        IAggregateRepository<TagParcelTemplate> repository,
+        IMapper mapper,
+        ICurrentUser currentUser,
+        ICurrentDateTime currentDateTime)
     {
-        
+        _repository = repository;
+        _mapper = mapper;
+        _currentUser = currentUser;
+        _currentDateTime = currentDateTime;
     }
     
-    public Task<Unit> Handle(SaveParcelTemplateCommand command, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(SaveParcelTemplateCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var spec = new TagParcelTemplateByTagAndTenantSpec(command.Tag, _currentUser.TenantId);
+        
+        var tagParcelTemplate = await _repository.SingleOrDefaultAsync(spec, cancellationToken);
+
+        var tagParcels = command.ParcelTemplates
+            .Select(x => new TagParcel(new Dimension(x.Dimensions.Length, x.Dimensions.Width, x.Dimensions.Height), x.Weight));
+        
+        if (tagParcelTemplate is null)
+        {
+            tagParcelTemplate = TagParcelTemplate.Create(command.Tag, tagParcels, _currentUser, _currentDateTime);
+            
+            await _repository.AddAsync(tagParcelTemplate, cancellationToken);
+        }
+        else
+        {
+            tagParcelTemplate.SetParcels(tagParcels);
+            
+            await _repository.SaveChangesAsync(cancellationToken);
+        }
+
+        return new Unit();
     }
 }
---
+++ src/TagParcelTemplates/AZ.Integrator.TagParcelTemplates.Domain/Aggregates/TagParcelTemplate/Specifications/TagParcelTemplateByTagAndTenantSpec.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+using AZ.Integrator.Domain.SharedKernel.ValueObjects;
+using AZ.Integrator.TagParcelTemplates.Domain.Aggregates.TagParcelTemplate.ValueObjects;
+
+namespace AZ.Integrator.TagParcelTemplates.Domain.Aggregates.TagParcelTemplate.Specifications;
+
+public sealed class TagParcelTemplateByTagAndTenantSpec : Specification<TagParcelTemplate>, ISingleResultSpecification<TagParcelTemplate>
+{
+    public TagParcelTemplateByTagAndTenantSpec(Tag tag, TenantId tenantId)
+    {
+        Query
+            .Where(x => x.Tag == tag.Value)
+            .Where(x => x.CreationInformation.TenantId == tenantId.Value)
+            .Include(x => x.Parcels);
+    }
+}
