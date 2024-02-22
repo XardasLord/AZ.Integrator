3c4b34b355d2e9c994c87bfa46d5f40f882c2234
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2024-02-22 18:56:40 +0100

    Extract Building Number and Street name from full address for shipment form

--- src/AZ.Integrator.Client/src/app/features/allegro-orders/pages/register-shipment-modal/register-shipment-modal.component.ts
+++ src/AZ.Integrator.Client/src/app/features/allegro-orders/pages/register-shipment-modal/register-shipment-modal.component.ts
@@ -53,11 +53,13 @@ export class RegisterShipmentModalComponent {
         [Validators.required, Validators.pattern('[0-9]{9}')]
       ),
       receiverAddressStreet: new FormControl<string>(
-        allegroOrderDetails.delivery?.address?.street ?? allegroOrderDetails.delivery.address.street,
+        this.extractStreetName(allegroOrderDetails.delivery?.address?.street) ??
+          this.extractStreetName(allegroOrderDetails.delivery.address.street),
         [Validators.required]
       ),
       receiverAddressBuildingNumber: new FormControl<string>(
-        allegroOrderDetails.delivery?.address?.street ?? allegroOrderDetails.delivery.address.street,
+        this.extractBuildingNumber(allegroOrderDetails.delivery?.address?.street) ??
+          this.extractBuildingNumber(allegroOrderDetails.delivery.address.street),
         [Validators.required]
       ),
       receiverAddressCity: new FormControl<string>(
@@ -317,5 +319,18 @@ export class RegisterShipmentModalComponent {
 
   private normalizePhoneNumber(phoneNumber: string) {
     return phoneNumber.replace('+48', '').replace(/ /g, '');
+  }
+
+  private extractStreetName(address: string): string {
+    const regex = /^(.*?)(?=\s*\d+\w*(\/\w+)?$)/;
+    const match = address.match(regex);
+    return match ? match[1].trim() : address;
+  }
+
+  private extractBuildingNumber(address: string): string {
+    const regex = /\d+\w*/;
+    const match = address.match(regex);
+
+    return match ? match[0] : '';
   }
 }
