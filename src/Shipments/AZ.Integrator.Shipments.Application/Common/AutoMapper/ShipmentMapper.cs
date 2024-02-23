4151cf1b2d2f0c607537c49989734abc66a8939b
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2024-02-23 09:53:41 +0100

    Uppercase Inpost Shipment's receiver data

--- src/Shipments/AZ.Integrator.Shipments.Application/Common/AutoMapper/ShipmentMapper.cs
+++ src/Shipments/AZ.Integrator.Shipments.Application/Common/AutoMapper/ShipmentMapper.cs
@@ -31,6 +31,23 @@ public class ShipmentMapper : Profile
                     CountryCode = senderAddressData.CountryCode
                 }
             }))
+            .ForMember(dest => dest.Receiver, opt => opt.MapFrom(src => new Receiver
+            {
+                Name = src.Receiver.Name == null ? null : src.Receiver.Name.ToUpper(),
+                CompanyName = src.Receiver.CompanyName == null ? null : src.Receiver.CompanyName.ToUpper(),
+                FirstName = src.Receiver.FirstName == null ? null : src.Receiver.FirstName.ToUpper(),
+                LastName = src.Receiver.LastName == null ? null : src.Receiver.LastName.ToUpper(),
+                Email = src.Receiver.Email == null ? null : src.Receiver.Email.ToUpper(),
+                Phone = src.Receiver.Phone == null ? null : src.Receiver.Phone.ToUpper(),
+                Address = new Address
+                {
+                    Street = src.Receiver.Address.Street == null ? null : src.Receiver.Address.Street.ToUpper(),
+                    BuildingNumber = src.Receiver.Address.BuildingNumber == null ? null : src.Receiver.Address.BuildingNumber.ToUpper(),
+                    City = src.Receiver.Address.City == null ? null : src.Receiver.Address.City.ToUpper(),
+                    PostCode = src.Receiver.Address.PostCode == null ? null : src.Receiver.Address.PostCode.ToUpper(),
+                    CountryCode = src.Receiver.Address.CountryCode == null ? null : src.Receiver.Address.CountryCode.ToUpper()
+                }
+            }))
             .ForMember(dest => dest.Service, opt => opt.MapFrom(src => "inpost_courier_standard"))
             .ForMember(dest => dest.AdditionalServices, opt => opt.MapFrom(src => new List<string>
             {
