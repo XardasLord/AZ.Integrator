0045d7f4e4b5f63678dafd4a728c2f6c30d2abbd
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-02-12 09:30:35 +0100

    Improve UI + update packages

--- src/AZ.Integrator.Client/src/app/core/ui/navigation/navigation.component.ts
+++ src/AZ.Integrator.Client/src/app/core/ui/navigation/navigation.component.ts
@@ -9,6 +9,14 @@ import { NgIf } from '@angular/common';
 import { AuthRoleAllowDirective } from '../../../shared/auth/directives/auth-role-allow.directive';
 import { MaterialModule } from '../../../shared/modules/material.module';
 
+export type NavigationItem = {
+  title: string;
+  icon: string;
+  route: string;
+  roles: AuthRoles[];
+  subItems?: NavigationItem[];
+};
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -20,6 +28,39 @@ export class NavigationComponent {
 
   RoutePaths = RoutePaths;
   AuthRoles = AuthRoles;
+
+  navigationItems: NavigationItem[] = [
+    {
+      title: 'Strona główna',
+      icon: 'home',
+      route: RoutePaths.Home,
+      roles: [],
+    },
+    {
+      title: 'Zamówienia',
+      icon: 'shopping_cart',
+      route: RoutePaths.Orders,
+      roles: [AuthRoles.Admin],
+    },
+    {
+      title: 'Szablony paczek',
+      icon: 'inventory',
+      route: RoutePaths.ParcelTemplates,
+      roles: [AuthRoles.Admin],
+    },
+    {
+      title: 'Stany magazynowe',
+      icon: 'warehouse',
+      route: RoutePaths.Stocks,
+      roles: [AuthRoles.Admin],
+    },
+    {
+      title: 'Skanowanie kodów',
+      icon: 'qr_code_scanner',
+      route: RoutePaths.BarcodeScanner,
+      roles: [AuthRoles.ScannerIn, AuthRoles.ScannerOut],
+    },
+  ];
 
   get appVersion() {
     return environment.version;
