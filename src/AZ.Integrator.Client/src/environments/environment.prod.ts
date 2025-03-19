73c2682feffca4558532222ec8512c732c4347e5
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-03-19 21:16:31 +0100

    Adjust production environments

--- src/AZ.Integrator.Client/src/environments/environment.prod.ts
+++ src/AZ.Integrator.Client/src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ import { APP_VERSION } from '../app/version';
 
 export const environment = {
   version: `${APP_VERSION}`,
-  apiEndpoint: 'http://SERWER:6010/api',
-  graphqlEndpoint: 'http://SERWER:6010/api/graphql',
-  keycloakEndpoint: 'http://SERWER:9080',
+  apiEndpoint: 'https://SERWER:6010/api',
+  graphqlEndpoint: 'https://SERWER:6010/api/graphql',
+  keycloakEndpoint: 'https://SERWER:9080',
   defaultPageSize: 10,
 
   // Tenants
-  allegroLoginEndpoint: 'http://SERWER:6010/api/auth/login-allegro?tenantId=',
+  allegroLoginEndpoint: 'https://SERWER:6010/api/auth/login-allegro?tenantId=',
 
   allegroAzTeamTenantId: 'allegro-az-team',
   allegroMebleplTenantId: 'allegro-meblepl',
