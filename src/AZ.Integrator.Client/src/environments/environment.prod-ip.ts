e0e622ec38d02e61045f01eb7dfb45d1510cbe3c
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-07-07 07:43:56 +0200

    Adjust production client deployment for DNS and IP configs (PC and scanners)

---
+++ src/AZ.Integrator.Client/src/environments/environment.prod-ip.ts
@@ -0,0 +1,22 @@
+import { APP_VERSION } from '../app/version';
+
+export const environment = {
+  version: `${APP_VERSION}`,
+  apiEndpoint: 'https://192.168.1.250:6011/api',
+  graphqlEndpoint: 'https://192.168.1.250:6011/api/graphql',
+  keycloakEndpoint: 'https://192.168.1.250:9080',
+  defaultPageSize: 10,
+
+  // Tenants
+  allegroLoginEndpoint: 'https://192.168.1.250:6011/api/auth/login-allegro?tenantId=',
+
+  allegroAzTeamTenantId: 'allegro-az-team',
+  allegroMebleplTenantId: 'allegro-meblepl',
+  allegroMyTestTenantId: 'allegro-my-test',
+  erliAzTeamTenantId: 'erli-az-team',
+
+  showMyTestAccounts: false,
+
+  // Stocks
+  stockWarningThreshold: 20,
+};
