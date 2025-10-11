0d99812f89e5fac67b59c09983855e8798e0205a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2025-10-11 20:57:51 +0200

    WIP - Catalog module introduction

---
+++ src/Modules/Catalog/AZ.Integrator.Catalog.Domain/Aggregates/FurnitureModel/FurnitureModel.cs
@@ -0,0 +1,17 @@
+﻿using AZ.Integrator.Domain.SeedWork;
+
+namespace AZ.Integrator.Catalog.Domain.Aggregates.FurnitureModel;
+
+public class FurnitureModel : Entity, IAggregateRoot
+{
+    private List<PartDefinition> _partDefinitions;
+    
+    public IReadOnlyCollection<PartDefinition> PartDefinitions => _partDefinitions;
+
+    private FurnitureModel()
+    {
+        _partDefinitions = [];
+    }
+    
+    
+}
---
+++ src/Modules/Catalog/AZ.Integrator.Catalog.Domain/Aggregates/FurnitureModel/PartDefinition.cs
@@ -0,0 +1,10 @@
+﻿using AZ.Integrator.Domain.SeedWork;
+
+namespace AZ.Integrator.Catalog.Domain.Aggregates.FurnitureModel;
+
+public class PartDefinition : Entity
+{
+    private string _name;
+
+    public string Name => _name;
+}
